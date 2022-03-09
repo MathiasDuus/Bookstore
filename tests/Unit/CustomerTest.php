c31930c1502d4a0b97c00bd9c55a669c26bfd567
Mathias Duus <math864n@edu.sde.dk>
2022-03-09 08:49:40 +0100

    V 0.2.0 Added Vue 3

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -24,4 +24,13 @@ class CustomerTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_customer_token()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiYjZmOWM0MDdmMzc2ZDk2YTdiMjU4NGNjYmIwYjA0NjhlZTAzNzYyNzRkMmI3MzU1YzQ1YjI1MDNlZjNmY2FhNDQ4MGI0Y2Y2NzdkYzU0M2YiLCJpYXQiOjE2NDY2Mzg4MzguNDY4NTI4LCJuYmYiOjE2NDY2Mzg4MzguNDY4NTMxLCJleHAiOjE2NTIzNDEyMzguMjU2MjU3LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.M1DCbuanPFt44SJ85g8qqyzt4paMFyIrRSRzOI7xXYjX9egTU_DUL044V_QoTLrwv8gUlKuJ9_cMGIyT9CKIpFMaXEG9jWKQO_i5yeT6HOwbst6snJ0Ub-BtezQyjgFP4ZvpvdOgbRO5Iw7qcebiKVrNpFIUZni-_zufWATZzRC0xvukTcaMXfESLypEhNIsPGcU5RkaHYzdSBGLZWytIFzSsgmzF6JqwW-dK6cF2rmtEslfJELQR0F2bTf20C_LcFYEOitP8fisB0yjsEZIXnEB5t0lp-zwvZ9hXS53VQwe-pZOR7SypeLwPvKqOGTxoO24Ost5hD4TdTuPBtMgXJ7FnwCv-9Zjpnwx9teAhmi0rqsER45N6WcUFZDiDAb7Xh7LCW9-OWJ3w7G_gg4zlmDjK8UwtIPm34ZXpIt8KXLM1Sf6gGzai7NNYazXHY45pgZ1XuybudLXoJgae42SR5ngeyCPY-4s1oQQ96d6M3zfGbeuPvu2AMUuCldTbKdE3XBN0cNooK8V96opqh3inLa9HfwjN4ma0n1rmLDzVYsKMJiovoXKFAYMZNAdYZCO1vzifUT_K1WCTHpiC5-KASF4YCeC4eXnwxsLoHdNSsei-Xok0bSGYFu40SuROwcEcQblXmf79gHCS__26mELki9FXEPKm9Dq5J61Zo2yJEI',
+        ])->get('/api/customer_token_test');
+
+        $response->assertStatus(200);
+    }
 }
